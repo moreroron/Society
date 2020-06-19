efa57d20318b455153001d62c96330aaa4424a9f
Maor Yadin <maoryadin0888@gmail.com>
2020-06-19 19:10:47 +0300

    RegisterActivity is now able to register through firebase

--- app/src/main/java/com/example/society/MainActivity.java
+++ app/src/main/java/com/example/society/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.society;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mUsername,mEmail,mPassword;
+    Button mRegisterBtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
+
+
     }
 }
--- app/src/main/java/com/example/society/RegisterActivity.java
+++ app/src/main/java/com/example/society/RegisterActivity.java
@@ -1,16 +1,84 @@
 package com.example.society;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText mEmail,mPassword;
+    Button mRegisterBtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmail = (EditText) findViewById(R.id.editTextEmail);
+        mPassword = (EditText) findViewById(R.id.editTextPassword);
+        mRegisterBtn = (Button) findViewById(R.id.RegisterButton);
+
+        fAuth = FirebaseAuth.getInstance();
+
+//
+//        if(fAuth.getCurrentUser() != null) {
+//            Intent intent = new Intent(this,MainActivity.class);
+//            startActivity(intent);
+//        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+
+                }
+                if(password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters.");
+                    return;
+
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error ! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+            }
+        });
 
     }
 }
